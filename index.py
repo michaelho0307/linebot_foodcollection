0034179c31f708cf27da90d609c613b1a9ccd873
MichaelHo <michaelho0307@gmail.com>
2022-08-17 01:02:00 +0800

    config

--- index.py
+++ index.py
@@ -7,12 +7,13 @@ from linebot.exceptions import InvalidSignatureError
 
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(
-    'dwzJBC7/J1LV0BFLiuWFNQay6/b+VU4O7FfuWexUPrOvHVKgzxhJaeSB0PcqJ13s+xuPca5neo4eQnCq7T/rcrZyTVJY06HlQ+8phJPchWL/fxRLZOO9FuA6NJ/y683Cxqd+GsltIOKEQEeWjfstlAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('b212af754eba3e46229e1d1b7f531121')
+    ChannelAccessToken)
+handler = WebhookHandler(ChannelSecret)
 
 # 接收 LINE 的資訊
 
