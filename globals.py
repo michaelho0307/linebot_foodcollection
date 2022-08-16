7c599b730e31571ba19ecbacf85ddaf6783c3c4f
MichaelHo <michaelho0307@gmail.com>
2022-08-17 01:15:41 +0800

    no globals

--- globals.py
+++
@@ -1,3 +0,0 @@
-ChannelAccessToken = 'dwzJBC7/J1LV0BFLiuWFNQay6/b+VU4O7FfuWexUPrOvHVKgzxhJaeSB0PcqJ13s+xuPca5neo4eQnCq7T/rcrZyTVJY06HlQ+8phJPchWL/fxRLZOO9FuA6NJ/y683Cxqd+GsltIOKEQEeWjfstlAdB04t89/1O/w1cDnyilFU='
-ChannelSecret = 'b212af754eba3e46229e1d1b7f531121'
-WebHook = "https://foodcollection.herokuapp.com/callback"
