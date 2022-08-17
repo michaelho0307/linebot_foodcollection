875e5041dd2d505ea18a197f3cff18a0ea1aba17
MichaelHo <michaelho0307@gmail.com>
2022-08-18 02:25:35 +0800

    recover_ver2

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from linebot.exceptions import InvalidSignatureError
 
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 #pymongo
-import pymongo
+#import pymongo
 
 app = Flask(__name__)
 
