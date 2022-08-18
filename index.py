fac47a73787ab6257a752e915b886fcca9c12d12
MichaelHo <michaelho0307@gmail.com>
2022-08-18 11:09:59 +0800

    0818

--- index.py
+++ index.py
@@ -21,10 +21,8 @@ handler = WebhookHandler(os.environ["ChannelSecret"])
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -33,14 +31,13 @@ def callback():
     return 'OK'
 
 
-# 學你說話
+# talk
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 
 if __name__ == "__main__":
