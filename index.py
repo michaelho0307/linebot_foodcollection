efd07de7262513fc1c393054d4e1929f223cf364
LouisTsai-Csie <Q1030176@gmail.com>
2022-08-28 20:56:40 +0800

    check collection name

--- index.py
+++ index.py
@@ -10,14 +10,14 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from flask import Flask, request, abort
 import pymongo
 import os
+import re
 
 app = Flask(__name__)
 
 # LINE BOT basic info
 line_bot_api = LineBotApi(os.environ["ChannelAccessToken"])
 handler = WebhookHandler(os.environ["ChannelSecret"])
-client = pymongo.MongoClient("mongodb+srv://michaelho:root@cluster0.kgvqwtd.mongodb.net/?retryWrites=true&w=majority")
-
+cluster = pymongo.MongoClient("mongodb+srv://michaelho:root@cluster0.kgvqwtd.mongodb.net/?retryWrites=true&w=majority")
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -34,11 +34,23 @@ def callback():
 # talk
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
-    msg = str(event.source.type).strip()
-    print(msg)
-    #profile = line_bot_api.get_profile(event.source.user_id)
-    #username = profile.display_name
-    #uid = profile.user_id
+    msg = str(event.message.text).strip()
+    source_type = event.source.type
+    print(cluster)
+
+    if source_type == 'user':
+        profile = line_bot_api.get_profile(event.source.user_id)
+        user_name = profile.display_name
+        uid = profile.user_id
+
+        user = userInfo.getUser(uid)
+
+
+
+    
+    elif source_type == 'group':
+        pass
+    
 
 
 
