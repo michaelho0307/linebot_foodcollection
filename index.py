5d548ec2c9895e8dfa017a9e12b5ad82dac9f59c
LouisTsai-Csie <Q1030176@gmail.com>
2022-08-28 19:19:52 +0800

    test mongodb

--- index.py
+++ index.py
@@ -1,19 +1,20 @@
-# modules
-from __future__ import unicode_literals
-import os
-from flask import Flask, request, abort
+### modules
+# line bot sdk
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-#pymongo
-#import pymongo
+
+
+from __future__ import unicode_literals
+from flask import Flask, request, abort
+import os
 
 app = Flask(__name__)
 
 # LINE bot basic info
 line_bot_api = LineBotApi(os.environ["ChannelAccessToken"])
 handler = WebhookHandler(os.environ["ChannelSecret"])
+client = pymongo.MongoClient("mongodb+srv://michaelho:root@cluster0.kgvqwtd.mongodb.net/?retryWrites=true&w=majority")
 #myclient = pymongo.MongoClient("mongodb+srv://michaelho:root@cluster0.kgvqwtd.mongodb.net/?retryWrites=true&w=majority")
 # 接收 LINE 的資訊
 
@@ -38,6 +39,7 @@ def echo(event):
         event.reply_token,
         TextSendMessage(text=event.message.text)
     )
+    print(client)
 
 
 if __name__ == "__main__":
