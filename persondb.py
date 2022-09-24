814562f5170b39c342a2d128baca26e75bf5ff95
hensuu-WSL <lsu900921@gmail.com>
2022-09-24 18:48:40 +0800

    test english action uri

--- persondb.py
+++ persondb.py
@@ -104,7 +104,7 @@ def get_resaurant(personSchema, LineID):
         restaurant_dict = {
             'name': name,
             'num': num,
-            'uri': f'https://www.google.com/'
+            'uri': f'https://www.google.com/search?q=test'
         }
         restaurant_list.append(restaurant_dict)
     return restaurant_list
